09923d51b08bee859a9b4e27e2a4b398cb215fc0
Mike Feole <blrg891@gmail.com>
2011-10-13 17:16:20 -0400

    Added game menus and classes

--- src/edu/uml/ThumbsUp/ThumbsUpActivity.java
+++ src/edu/uml/ThumbsUp/ThumbsUpActivity.java
@@ -2,7 +2,9 @@ package edu.uml.thumbsup;
 
 import edu.uml.thumbsup.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,16 +12,24 @@ public class ThumbsUPActivity extends Activity {
     /** Called when the activity is first created. */
     
     Button bNewGame, bHighScores, bOptions, bCredits ;
-    TextView mainDisplay ;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        bNewGame = (Button) findViewById(R.id.button1);
-        bHighScores = (Button) findViewById(R.id.Button01);
-        bOptions = (Button) findViewById(R.id.Button02);
-        bCredits = (Button) findViewById(R.id.Button03);
-        mainDisplay = (TextView) findViewById(R.id.textView1);
+        bNewGame = (Button) findViewById(R.id.mbutton1);
+        bHighScores = (Button) findViewById(R.id.mButton01);
+        bOptions = (Button) findViewById(R.id.mButton02);
+        bCredits = (Button) findViewById(R.id.mButton03);
+        
+        bNewGame.setOnClickListener(new View.OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		
+				Intent openGameMenu = new Intent("edu.uml.thumbsup.GAMEMENU");
+				startActivity(openGameMenu);
+        		
+        	}
+        });        
     }
 }
---
+++ src/edu/uml/ThumbsUp/game5.java
@@ -0,0 +1,15 @@
+package edu.uml.thumbsup;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class game5 extends Activity{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.game5);
+	}
+	
+}
