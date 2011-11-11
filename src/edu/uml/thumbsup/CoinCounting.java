0955ca74c2517cf3f83fe78b86f26f97a8478740
Elad Shahar <SaladFork@Gmail.com>
2011-11-10 22:00:12 -0500

    Added Coin Counting files

---
+++ src/edu/uml/thumbsup/CoinCounting.java
@@ -0,0 +1,13 @@
+package edu.uml.thumbsup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CoinCounting extends Activity{
+
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.coincounting);
+	}
+	
+}
