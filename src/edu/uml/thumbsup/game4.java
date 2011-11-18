e746808c4000ce7b42aca1eb0b2075094f16dd0f
Sunil BT <sunil.rnsit@gmail.com>
2011-11-17 21:41:50 -0500

    Updated function repeat

--- src/edu/uml/thumbsup/game4.java
+++ src/edu/uml/thumbsup/game4.java
@@ -3,10 +3,12 @@ package edu.uml.thumbsup;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Vector;
-
-
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -18,6 +20,8 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class game4 extends Activity {
 	
@@ -29,6 +33,57 @@ public class game4 extends Activity {
 	Vector<String> alreadyUsed = new Vector<String>();
 	int count = 0; 
 	long timer;
+	
+	
+	public void repeat(){
+	    Resources res = getResources();      
+        //fetches values from the array in string.xml
+	    color = res.getStringArray(R.array.colors);
+       
+	    // selects a random value out of color string array Ex: color[5], color[3]   	    
+	    do{
+		    colorSelect = color[myRandom.nextInt(color.length)];
+	        } while(alreadyUsed.contains(colorSelect));
+	        alreadyUsed.add(colorSelect);
+     
+	        
+        TextView tv = (TextView)findViewById(R.id.generatecolor);
+        tv.setText(colorSelect);
+        
+        if(alreadyUsed.size() == 12){
+        	
+        	int scorevalue = scores();
+        	
+	        Log.v(LOG_TAG, "size:" + alreadyUsed.size());
+	        Log.v(LOG_TAG, "mistakes:" + count);
+	        
+	        // Create the alert box
+            AlertDialog.Builder alertbox = new AlertDialog.Builder(this);
+            // Set the message to display
+            alertbox.setMessage("You Completed with a score = " + scorevalue);
+            // Add a neutral button to the alert box and assign a click listener
+            	alertbox.setNeutralButton("EXIT", new DialogInterface.OnClickListener() {
+            		//Click listener on the neutral button of alert box
+            		  public void onClick(DialogInterface arg0, int arg1) {
+                      // The neutral button was clicked
+                     Toast.makeText(getApplicationContext(), "'EXIT' button clicked", Toast.LENGTH_LONG).show();
+                     //finish();
+                     bgSong.release();
+                     finish();
+                     }
+               });
+            
+            // show the alert box
+            alertbox.show();
+            Log.v(LOG_TAG, "size:" + alreadyUsed.size());
+	        }	   
+  }
+	
+	int scores(){
+		return 0;
+	}
+	
+	
 	
 		public void onCreate(Bundle savedInstanceState){
 			super.onCreate(savedInstanceState);
