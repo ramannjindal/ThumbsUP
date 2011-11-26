cea2e002a8c7645b61a341f9ace045ea48466828
Caleb Brandon <obi.wan.calobi@gmail.com>
2011-11-25 23:01:27 -0500

    I changed the game menu, to be more like the main menu.  I'll send out an email to see what people's thoughts on it are.

--- src/edu/uml/thumbsup/gamemenu.java
+++ src/edu/uml/thumbsup/gamemenu.java
@@ -7,76 +7,102 @@ import android.view.View;
 import android.widget.Button;
 
 public class gamemenu extends Activity{
-
-	Button game1, game2, game3, game4, game5, game6, coinCounting, game8;
+	int gameNumber = 0;
+	static int totalGames = 8;
+	Button bSelectedGame, bArrowRight, bArrowLeft;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
         setContentView(R.layout.gamemenu);
-        game1 = (Button)findViewById(R.id.gmbutton1);
-        game2 = (Button)findViewById(R.id.gmbutton2);
-        game3 = (Button)findViewById(R.id.gmbutton3);
-        game4 = (Button)findViewById(R.id.gmbutton4);
-        game5 = (Button)findViewById(R.id.gmbutton5);
-        game6 = (Button)findViewById(R.id.gmbutton6);
-        coinCounting = (Button)findViewById(R.id.gmbutton_coincounting);
-        game8 = (Button)findViewById(R.id.gmbutton8);
+        
+        bSelectedGame = (Button) findViewById(R.id.game_button);
+        bSelectedGame.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View v) {
+        		LaunchSelectedGame();
+        	}
+        });
+        
+        bArrowRight = (Button) findViewById(R.id.arrow_right_button);
+        bArrowRight.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View v) {
+        		gameNumber = (gameNumber + 1) % totalGames;
+        		ChangeCurrentButtonText();
+        	}
+        });
+        
+        bArrowLeft = (Button) findViewById(R.id.arrow_left_button);
+        bArrowLeft.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View v) {
+        		gameNumber = (gameNumber - 1) % totalGames;
+        		ChangeCurrentButtonText();
+        	}
+        });
+	}
 	
-        game1.setOnClickListener(new View.OnClickListener() {
-        	public void onClick(View v) {
-				Intent openGame1 = new Intent("edu.uml.thumbsup.GAME1");
-				startActivity(openGame1);			
-        	}
-        });
-        
-        game2.setOnClickListener(new View.OnClickListener() {
-        	public void onClick(View v) {
-				Intent openGame2 = new Intent("edu.uml.thumbsup.GAME2");
-				startActivity(openGame2);			
-        	}
-        });
-        
-        game3.setOnClickListener(new View.OnClickListener() {
-        	public void onClick(View v) {
-				Intent openGame3 = new Intent("edu.uml.thumbsup.GAME3");
-				startActivity(openGame3);			
-        	}
-        });
-        
-        game4.setOnClickListener(new View.OnClickListener() {
-        	public void onClick(View v) {
-				Intent openGame4 = new Intent("edu.uml.thumbsup.GAME4");
-				startActivity(openGame4);			
-        	}
-        });
-        
-        game5.setOnClickListener(new View.OnClickListener() {
-        	public void onClick(View v) {
-				Intent openGame5 = new Intent("edu.uml.thumbsup.GAME5");
-				startActivity(openGame5);			
-        	}
-        });
-        
-        game6.setOnClickListener(new View.OnClickListener() {
-        	public void onClick(View v) {
-				Intent openGame6 = new Intent("edu.uml.thumbsup.GAME6");
-				startActivity(openGame6);			
-        	}
-        });
-        
-        coinCounting.setOnClickListener(new View.OnClickListener() {
-        	public void onClick(View v) {
-				Intent openGame7 = new Intent("edu.uml.thumbsup.COINCOUNTING");
-				startActivity(openGame7);			
-        	}
-        });
-        
-        game8.setOnClickListener(new View.OnClickListener() {
-        	public void onClick(View v) {
-				Intent openGame8 = new Intent("edu.uml.thumbsup.GAME8");
-				startActivity(openGame8);			
-        	}
-        });
+	private void ChangeCurrentButtonText(){
+		switch(gameNumber){
+		case 0:
+			bSelectedGame.setText(R.string.game1_name);
+			break;
+		case 1:
+			bSelectedGame.setText(R.string.game2_name);
+			break;
+		case 2:
+			bSelectedGame.setText(R.string.game3_name);
+			break;
+		case 3:
+			bSelectedGame.setText(R.string.game4_name);
+			break;
+		case 4:
+			bSelectedGame.setText(R.string.game5_name);
+			break;
+		case 5:
+			bSelectedGame.setText(R.string.game6_name);
+			break;
+		case 6:
+			bSelectedGame.setText(R.string.game7_name);
+			break;
+		case 7:
+			bSelectedGame.setText(R.string.game8_name);
+			break;
+		}
+	}
+	
+	private void LaunchSelectedGame(){
+		switch(gameNumber){
+		case 0:
+			Intent openGame1 = new Intent("edu.uml.thumbsup.GAME1");
+			startActivity(openGame1);
+			break;
+		case 1:
+			Intent openGame2 = new Intent("edu.uml.thumbsup.GAME2");
+			startActivity(openGame2);
+			break;
+		case 2:
+			Intent openGame3 = new Intent("edu.uml.thumbsup.GAME3");
+			startActivity(openGame3);
+			break;
+		case 3:
+			Intent openGame4 = new Intent("edu.uml.thumbsup.GAME4");
+			startActivity(openGame4);
+			break;
+		case 4:
+			Intent openGame5 = new Intent("edu.uml.thumbsup.GAME5");
+			startActivity(openGame5);
+			break;
+		case 5:
+			Intent openGame6 = new Intent("edu.uml.thumbsup.GAME6");
+			startActivity(openGame6);
+			break;
+		case 6:
+			Intent openGame7 = new Intent("edu.uml.thumbsup.GAME7");
+			startActivity(openGame7);
+			break;
+		case 7:
+			Intent openGame8 = new Intent("edu.uml.thumbsup.GAME8");
+			startActivity(openGame8);
+			break;
+		}
 	}
 }
