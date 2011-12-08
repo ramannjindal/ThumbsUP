47cd14f91a207d2d1cc496339e6d75e18cb4b804
Chris Adler <cadler42@gmail.com>
2011-12-08 02:00:07 -0500

    formatting of chem game code

--- src/edu/uml/thumbsup/ChemGameActivity.java
+++ src/edu/uml/thumbsup/ChemGameActivity.java
@@ -1,7 +1,6 @@
 package edu.uml.thumbsup;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.text.InputType;
