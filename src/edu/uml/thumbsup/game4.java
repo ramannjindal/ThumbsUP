d56be7f883bfe1e7c1c8e5f90abfb42684d9539c
Sunil BT <sunil.rnsit@gmail.com>
2011-12-08 05:17:49 -0500

    comments on game4

--- src/edu/uml/thumbsup/game4.java
+++ src/edu/uml/thumbsup/game4.java
@@ -1,5 +1,14 @@
 package edu.uml.thumbsup;
-
+/* Developer Name - Sunil Kumar Balaganchi Thammaiah
+ * UML ID - 01155593
+ * This app works on helping the kids recognize different colors
+ * 1. In this game, I have used 'TIMER' to calculate the points. 
+ * 	  Timer begins as soon as the game starts and ends on selection of the last Color.
+ * 2. Also worked with Ms.Prathiba Dyavegowda in getting the SQLite Database up and running.
+ * 3. Designed the 'HOME PAGE BACKGROUND' for the ThumbsUP Application
+ * 4. Created the 'BACKGROUND MUSIC' for the Home Page and Game Selection Activity
+ * 5. Setup the 'BUTTON BACKGROUND' for the Home Page and Game Selection Activity
+ */
 
 import java.util.Random;
 import java.util.Vector;
@@ -27,11 +36,13 @@ import android.widget.Toast;
 public class game4 extends Activity {
 	
 	public static final String LOG_TAG = "CHECKING";
-	MediaPlayer bgSong;
-	private String[] color;;
 	private static final Random myRandom = new Random();
+	
+	private String[] color;;
 	public String colorSelect;
 	Vector<String> alreadyUsed = new Vector<String>();
+	
+	MediaPlayer bgSong;
 	int count = 0; 
 	long timer;
 		
