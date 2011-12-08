f218ebc9ea7c621d89daf47840aa9d7275154977
Paul Gendreau <pdgendreau@yahoo.com>
2011-12-08 10:32:54 -0500

    done

--- src/edu/uml/thumbsup/game1m.java
+++ src/edu/uml/thumbsup/game1m.java
@@ -3,7 +3,6 @@ package edu.uml.thumbsup;
 
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -65,7 +64,7 @@ public class game1m extends Activity{
 }
    
     private void func(double z, String s) {
-    	if(z == .25){
+    	if(z == .28){
     	score += 100;
     	displayScore.setText("score " + score);
     	mdisplay.setText("you are correct ");
